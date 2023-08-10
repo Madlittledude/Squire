2323c6c259a8b7b5f6bcc2c05a823431fd1ee790
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-10 16:02:01 -0700

    Update auth.py

--- Scripts/auth.py
+++ Scripts/auth.py
@@ -1,15 +1,14 @@
 
-import os
+import streamlit as st
 
 def load_credentials():
-    # Load the TOML content from an environment variable
-    credentials = os.environ['users']
-  
+    # Load the credentials from Streamlit's secrets
+    credentials = st.secrets['users']
     return credentials
 
 def authenticate(username, password):
     credentials = load_credentials()
     # Check if the provided username and password match the stored credentials
-    if username in credentials['users'] and credentials['users'][username] == password:
+    if username in credentials and credentials[username] == password:
         return True
     return False
