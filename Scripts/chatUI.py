5511dc9e8c5f7788d53dd45d3c2c04f63e83c88f
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-10 17:34:42 -0700

    Update chatUI.py

--- Scripts/chatUI.py
+++ Scripts/chatUI.py
@@ -3,6 +3,7 @@ import openai
 import json
 import boto3
 from datetime import datetime
+from log import ChatLogger
 
 
 class ChatManager:
@@ -51,34 +52,12 @@ class ChatManager:
 
 
     def log_chat(self, user_message, assistant_message):
-        # Log the chat messages
-        chat_entry = {"user": user_message, "assistant": assistant_message}
-        if not self.chat_logs["chat_logs"]:
-            self.chat_logs["chat_logs"].append({
-                "day": datetime.now().strftime("%Y-%m-%d"),
-                "sessions": [{"session_start_time": datetime.now().strftime("%I:%M %p"), "chat": [chat_entry]}]
-            })
-        else:
-            current_day_log = self.chat_logs["chat_logs"][-1]
-            if current_day_log["day"] == datetime.now().strftime("%Y-%m-%d"):
-                current_day_log["sessions"][-1]["chat"].append(chat_entry)
-            else:
-                self.chat_logs["chat_logs"].append({
-                    "day": datetime.now().strftime("%Y-%m-%d"),
-                    "sessions": [{"session_start_time": datetime.now().strftime("%I:%M %p"), "chat": [chat_entry]}]
-                })
+        self.logger.log_chat(user_message, assistant_message)  # Call the log_chat method of ChatLogger
 
-    def save_chat_to_json(self):
-        # Save the chat logs to a JSON file
-        filename = 'test1.json'
-        with open(filename, 'w') as file:
-            json.dump(self.chat_logs, file)
+   def save_chat_to_json(self):
+    self.logger.save_chat_to_json()  # Call the save_chat_to_json method of ChatLogger
 
-        # Upload the JSON file to S3
-        client = boto3.client('s3', aws_access_key_id="ACCESS_KEY", aws_secret_access_key="SECRET_KEY")
-        upload_file_bucket = 'brainstormdata'
-        upload_file_key = str('test1.json')
-        client.upload_file(filename, upload_file_bucket, upload_file_key)
+
         
 def display_chat_message(role, content, avatar):
     with st.chat_message(role, avatar=avatar):
