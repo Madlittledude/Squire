08acc1ad22edf59180cbbb1befa903295b407bed
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-19 20:23:30 -0700

    Update log.py

--- Scripts/log.py
+++ Scripts/log.py
@@ -1,18 +1,12 @@
-import boto3
-import json
-from datetime import datetime
-import os
-
 class ChatLogger:
     def __init__(self, username):
         self.username = username
         self.date = datetime.utcnow().strftime('%Y-%m-%d')
         self.client = boto3.client('s3',
-                           region_name='us-west-1',
-                           aws_access_key_id=os.environ["ACCESS_KEY"],
-                           aws_secret_access_key=os.environ["SECRET_KEY"])
+                                   region_name='us-west-1',
+                                   aws_access_key_id=os.environ["ACCESS_KEY"],
+                                   aws_secret_access_key=os.environ["SECRET_KEY"])
         self.load_existing_logs()
-        self.start_new_session()
 
     def load_existing_logs(self):
         bucket_name = 'brainstormdata'
@@ -23,34 +17,16 @@ class ChatLogger:
         except:
             self.chat_history = {"user_id": self.username, "logs": []}
         today_log = [log for log in self.chat_history["logs"] if log["date"] == self.date]
-        self.session_count = len(today_log[0]["sessions"]) if today_log else 0
-
-    def start_new_session(self):
-        session_data = {
-            "session_id": f"session{self.session_count + 1}",
-            "messages": []
-        }
-        today_log = [log for log in self.chat_history["logs"] if log["date"] == self.date]
-        if today_log:
-            today_log[0]["sessions"].append(session_data)
-        else:
-            date_log = {
-                "date": self.date,
-                "sessions": [session_data]
-            }
-            self.chat_history["logs"].append(date_log)
-        self.session_count += 1
+        if not today_log:
+            self.chat_history["logs"].append({"date": self.date, "messages": []})
 
     def log_chat(self, user_message, assistant_message):
         time_now = datetime.utcnow().strftime('%H:%M:%S')
+        today_log = [log for log in self.chat_history["logs"] if log["date"] == self.date][0]
         if user_message:
-            self.chat_history["logs"][-1]["sessions"][-1]["messages"].append(
-                {"sender": "user", "message": user_message, "time": time_now}
-            )
+            today_log["messages"].append({"sender": "user", "message": user_message, "time": time_now})
         if assistant_message:
-            self.chat_history["logs"][-1]["sessions"][-1]["messages"].append(
-                {"sender": "assistant", "message": assistant_message, "time": time_now}
-            )
+            today_log["messages"].append({"sender": "assistant", "message": assistant_message, "time": time_now})
 
     def save_chat_to_s3(self):
         # Save the chat history to S3 bucket
